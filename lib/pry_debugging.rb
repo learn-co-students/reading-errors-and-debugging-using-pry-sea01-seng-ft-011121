97375949a2774d686c645813edfa2de1e76abca7
Adan Escamilla <adanescamilla580@gmail.com>
2020-12-22 17:53:00 +0000

    Done.

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,8 +1,5 @@
+
 def plus_two(num)
-    num + 2
-    num
+   num = num + 2
   end
   
-  I was not able to do this lab because the code in spec 
-  was not running and no one was able 
-  to help me out and see how to fix it. 
